bc9e48742f497dc546ec9cb3266fe50400fb95db
Shiva Iyer <shiva.iyer@utexas.edu>
2019-02-25 09:33:54 -0600

    Added GPL v3 license headers.

--- celemech.js
+++ celemech.js
@@ -1,7 +1,19 @@
 /*
- * Author : Shiva Iyer (shiva.iyer AT utexas DOT edu)
+ * celemech.js - Celestial mechanics conversion routines.
+ * Copyright (C) 2019 Shiva Iyer <shiva.iyer AT utexas DOT edu>
  *
- * Routines for various tasks in celestial mechanics.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 var J2000Epoch = 2451545.0
